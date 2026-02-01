c1ce17a50abd0f70529faf5abbd109e640ca2906
Rayhan Islam <merayhanislam21@gmail.com>
2026-02-01 20:41:59 +0600

    added redux react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//step 1: install redux and react-redux and react thunk
+//step 2: constant define
+//step 3: action create
+//step 4: reducer create
+//step 5: store create
+//step 6: provide the store to app
+//step 7: use the store in component
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Redux App</h1>
+    </div>
+  );
+};
+
+export default App;
