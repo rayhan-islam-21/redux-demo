34a0ef46ed83ad7e8ee3152620e7f58e465ba0f7
Rayhan Islam <merayhanislam21@gmail.com>
2026-02-02 15:42:00 +0600

    todays first

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 //step 1: install redux and react-redux and react thunk
 //step 2: constant define
@@ -7,13 +7,21 @@ import React from 'react';
 //step 5: store create
 //step 6: provide the store to app
 //step 7: use the store in component
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const App = () => {
+
+  const store = useSelector(state => state);
+  console.log(store);
+                                                                                                                                                                                                                                                
+  
+
   return (
     <div>
       <h1>React Redux App</h1>
     </div>
   );
 };
-
-export default App;
+``
+export default App;    n                                                                                                                                                                             
